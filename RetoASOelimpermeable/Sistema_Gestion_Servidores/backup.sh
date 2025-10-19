d181fe6f0b3e81288d9126e6306c90bce469d489
elimpermeable <alemasboj@alu.edu.gva.es>
2025-10-19 22:45:21 +0200

    Actualización automática: 2025-10-19 22:45:21

--- RetoASOelimpermeable/Sistema_Gestion_Servidores/backup.sh
+++ RetoASOelimpermeable/Sistema_Gestion_Servidores/backup.sh
@@ -1,1 +1,14 @@
-jaleo
+#!/bin/bash
+# Copias de seguridad
+
+menu_backup() {
+    PS3="Elige una opción: "
+    select opcion in "Crear backup" "Restaurar backup" "Volver al menú principal"; do
+        case $opcion in
+            "Crear backup") crear_backup ;;
+            "Restaurar backup") restaurar_backup ;;
+            "Volver al menú principal") break ;;
+            *) echo "Opción no válida" ;;
+        esac
+    done
+}
