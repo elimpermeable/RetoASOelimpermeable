b5d6e57a4cf865800b2a3006015090869ffe5f87
elimpermeable <alemasboj@alu.edu.gva.es>
2025-10-19 23:05:40 +0200

    Update logs.sh

--- RetoASOelimpermeable/logs.sh
+++ RetoASOelimpermeable/logs.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
-# Logs
+# Gestión de logs
+
+LOG_ESTADO="estado_servidores.log"
+LOG_BACKUP="backup.log"
+
+menu_logs() {
+    PS3="Elige una opción: "
+    select opcion in "Ver logs" "Limpiar logs" "Volver al menú principal"; do
+        case $opcion in
+            "Ver logs") ver_logs ;; 
+            "Limpiar logs") limpiar_logs ;; 
+            "Volver al menú principal") break ;; 
+            *) echo "Opción no válida" ;; 
+        esac
+    done
+}
+
+ver_logs() {
+    cat $LOG_ESTADO $LOG_BACKUP
+}
+
+limpiar_logs() {
+    > $LOG_ESTADO
+    > $LOG_BACKUP
+    echo "Logs limpiados"
+}
